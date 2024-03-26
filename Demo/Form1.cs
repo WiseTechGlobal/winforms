2922f59cf49177e605d6911d933d9ffb78beb8dd
Ashish Manatosh <Ashish.Barik@wisetechglobal.com>
2024-03-26 12:42:34 +0530

    Added demo project

---
+++ Demo/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        private Button button1;
+        private Button button2;
+        private System.Windows.Forms.DataGrid dataGrid;
+        private DataSet myDataSet;
+        private bool TablesAlreadyAdded;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
