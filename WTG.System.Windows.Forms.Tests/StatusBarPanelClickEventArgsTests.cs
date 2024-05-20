fb1055986fa46c02c8d4598dfa7164599dc5b103
wangyang2024 <166356437+wangyang2024@users.noreply.github.com>
2024-05-20 10:28:00 +1000

    add status bar related tests (#6)
    WI00745296 - Port status bar tests to .net8 (PR 6)
    ## What I do in this PR:
    
    #### 1 Add 4 winform tests:
    
    MenuItemTests.cs
    MenuTests.cs
    StatusBarDrawItemEventArgsTests.cs
    StatusBarPanelClickEventArgsTests.cs
    
    #### 2 Disable 2 warnings
    #### 3 Import Moq, as some tests used **Mock**.
    
    
    ---
    I copied these 4 files from here:
    https://github.com/dotnet/winforms/blob/4.8/src/System.Windows.Forms/tests/UnitTests/MenuItemTests.cs
    https://github.com/dotnet/winforms/blob/4.8/src/System.Windows.Forms/tests/UnitTests/MenuTests.cs
    https://github.com/dotnet/winforms/blob/v3.1.27/src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/StatusBarDrawItemEventArgsTests.cs
    https://github.com/dotnet/winforms/blob/v3.1.27/src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/StatusBarPanelClickEventArgsTests.cs
    
    
    <img width="471" alt="image" src="https://github.com/WiseTechGlobal/CargoWise.Controls.WinForms.Legacy/assets/166356437/342f1369-eeca-4830-aeb4-db34e0812813">

---
+++ WTG.System.Windows.Forms.Tests/StatusBarPanelClickEventArgsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WTG.System.Windows.Forms.Tests
+{
+    public class StatusBarPanelClickEventArgsTests
+    {
+        public static IEnumerable<object[]> Ctor_StatusBarPanel_MouseButtons_Int_Int_Int_Int_TestData()
+        {
+            yield return new object[] { null, MouseButtons.Left, 1, 2, 3 };
+            yield return new object[] { new StatusBarPanel(), (MouseButtons)1, 0, 0, 0 };
+            yield return new object[] { new StatusBarPanel(), (MouseButtons)3, -1, -1, -1 };
+        }
+
+        [Theory]
+        [MemberData(nameof(Ctor_StatusBarPanel_MouseButtons_Int_Int_Int_Int_TestData))]
+        public void Ctor_StatusBarPanel_MouseButtons_Int_Int_Int_Int(StatusBarPanel statusBarPanel, MouseButtons button, int clicks, int x, int y)
+        {
+            var e = new StatusBarPanelClickEventArgs(statusBarPanel, button, clicks, x, y);
+            Assert.Equal(statusBarPanel, e.StatusBarPanel);
+            Assert.Equal(button, e.Button);
+            Assert.Equal(clicks, e.Clicks);
+            Assert.Equal(x, e.X);
+            Assert.Equal(y, e.Y);
+            Assert.Equal(0, e.Delta);
+            Assert.Equal(new Point(x, y), e.Location);
+        }
+    }
+}
