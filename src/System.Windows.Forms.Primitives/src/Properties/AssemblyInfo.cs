3357df6bb3ef9e86774709d4b2b9fd696a89f8ae
Mark <115767053+wtg-mg8@users.noreply.github.com>
2024-06-25 08:16:41 +1000

    WI00756869 - Change CargoWise.Controls.WinForms.Legacy to output System.Windows.Forms.dll (#13)
    WI00756869 - Change CargoWise.Controls.WinForms.Legacy to output System.Windows.Forms.dll (PR 13)
    WI00756869 - Change CargoWise.Controls.WinForms.Legacy to output System.Windows.Forms.dll

--- src/System.Windows.Forms.Primitives/src/Properties/AssemblyInfo.cs
+++ src/System.Windows.Forms.Primitives/src/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.CompilerServices;
 
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 
-[assembly: InternalsVisibleTo("WTG.System.Windows.Forms, PublicKey=00000000000000000400000000000000")]
+[assembly: InternalsVisibleTo("System.Windows.Forms, PublicKey=00000000000000000400000000000000")]
 [assembly: InternalsVisibleTo("System.Windows.Forms.Design, PublicKey=00000000000000000400000000000000")]
 [assembly: InternalsVisibleTo("System.Windows.Forms.Design.Editors, PublicKey=00000000000000000400000000000000")]
 
