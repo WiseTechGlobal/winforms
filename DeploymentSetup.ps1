c72c88c6fa6fce0d0a0b4552a03893767f7806b3
Mark <115767053+wtg-mg8@users.noreply.github.com>
2025-09-08 17:48:24 +1000

    WI00960762 - Update CargoWiseCloudDeployment package in CargoWise.Controls.WinForms.Legacy (#44)
    [WI00960762 - Update CargoWiseCloudDeployment package in CargoWise.Controls.WinForms.Legacy](https://ediprod.cw.wisetechglobal.com/link/ShowEditForm/WorkItem/05457ed6-e55a-449b-a34a-53da24161495?lang=en-gb)
    
    Update the `CargoWiseCloudDeployment` package to use the latest version, to fix deployment issue. See [Teams](https://teams.microsoft.com/l/message/19:5389c798f144428e9b9b1df20661e308@thread.tacv2/1757045159168?tenantId=8b493985-e1b4-4b95-ade6-98acafdbdb01&groupId=75b1866b-4460-4231-9c98-fd2580072cb4&parentMessageId=1757045159168&teamName=CW%20Modernization&channelName=CW%20.Net8%20Squad&createdTime=1757045159168) for background.

--- DeploymentSetup.ps1
+++ DeploymentSetup.ps1
@@ -1,9 +1,14 @@
 $pkgId = 'CargoWiseCloudDeployment'
-$pkgVersion = '1.0.0.4' # Update this to the latest version as needed
+
+# this uses the latest version of the package, as listed at https://proget.wtg.zone/packages?PackageSearch=CargoWiseCloudDeployment
+# if you need a specific version, you can use:
+#$pkgVersion = "1.0.0.24"
+#url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}/$pkgVersion"
+
 $Destination = '.\bin'
 
 $pkgRoot = Join-Path $Destination ("$pkgId.$pkgVersion")
-$url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}/${pkgVersion}"
+$url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}"
 try {
 	Invoke-WebRequest -Uri $url -OutFile "$pkgRoot.zip" -ErrorAction Stop
 	Write-Host "download nuget successfully"
