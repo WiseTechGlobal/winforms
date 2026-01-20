d3fb1d812da233e9a0d2a1dcd3d8f8c89c9c79fa
Michael Allen <163090374+MichaelAllen1@users.noreply.github.com>
2026-01-21 00:49:49 +1100

    WI01012522 - Updated DeploymentSetup.ps1 (#48)
    ### [WI01012522 - Upgrade Deployment Projects To Net Core](https://ediprod.cw.wisetechglobal.com/link/ShowEditForm/WorkItem/68ab5a57-7722-4e51-97dd-b8ccc03de75f?lang=en-gb)
    
    ----
    
    ### Description
    Update DeploymentSetup.ps1 for net core deployments

--- DeploymentSetup.ps1
+++ DeploymentSetup.ps1
@@ -2,13 +2,16 @@ $pkgId = 'CargoWiseCloudDeployment'
 
 # this uses the latest version of the package, as listed at https://proget.wtg.zone/packages?PackageSearch=CargoWiseCloudDeployment
 # if you need a specific version, you can use:
-#$pkgVersion = "1.0.0.24"
+#$pkgVersion = "1.0.0.70"
 #url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}/$pkgVersion"
 
 $Destination = '.\bin'
 
+$pkgVersion = "1.0.0.362" # Set this to $null for most recent package.
 $pkgRoot = Join-Path $Destination ("$pkgId.$pkgVersion")
-$url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}"
+$url = "https://proget.wtg.zone/nuget/WTG-Internal/package/${pkgId}/$pkgVersion"
+
+
 try {
 	Invoke-WebRequest -Uri $url -OutFile "$pkgRoot.zip" -ErrorAction Stop
 	Write-Host "download nuget successfully"
@@ -24,5 +27,9 @@ try {
 catch {
 	Write-Error "unzip failed: $($_.Exception.Message)"
 }
+
 Copy-Item -Path (Join-Path (Join-Path $pkgRoot '\content') '*') -Destination $Destination -Recurse -Force
-Copy-Item -Path (Join-Path (Join-Path $pkgRoot '\lib\net48') '*') -Destination $Destination -Recurse -Force
+Copy-Item -Path (Join-Path (Join-Path $pkgRoot '\lib\net10.0-windows7.0') '*') -Destination $Destination -Recurse -Force
+
+Remove-Item -Path "$pkgRoot.zip" -Force
+Remove-Item -Path $pkgRoot -Recurse -Force
