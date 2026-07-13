e6ae66f328837d9c0119e57385e0ce81a570b631
jay.wang <jay.wang@wisetechglobal.com>
2026-07-13 20:39:44 +0800

    revert the LayoutCompat

--- src/System.Windows.Forms.Legacy/System.Windows.Forms.Legacy.Demo/MainForm.cs
+++ src/System.Windows.Forms.Legacy/System.Windows.Forms.Legacy.Demo/MainForm.cs
@@ -1,4 +1,4 @@
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Demo;
 
@@ -30,12 +30,6 @@ public partial class MainForm : Form
     private void StatusBarButton_Click(object? sender, EventArgs e)
     {
         StatusBarForm form = new();
-        form.Show(this);
-    }
-
-    private void AnchorLayoutHighDpiRegressionButton_Click(object? sender, EventArgs e)
-    {
-        AnchorLayoutHighDpiRegressionDemoForm form = new();
         form.Show(this);
     }
 }
