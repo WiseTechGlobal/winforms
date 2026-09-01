ee5a4a9986932ffb9090326c1b7fd964ab80d46f
jay.wang <148020729+jaywang-cn@users.noreply.github.com>
2026-09-01 15:43:27 +0800

    WI01118904 - Handle disposed AsyncWaitHandle during completion (#42)
    * Handle disposed AsyncWaitHandle during BeginInvoke completion
    
    * Adapt AsyncWaitHandle test to fork message loop
    
    * update the package description

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/ControlTests.Methods.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/ControlTests.Methods.cs
@@ -3851,6 +3851,21 @@ public partial class ControlTests
     }
 
     [WinFormsFact]
+    public void Control_BeginInvoke_DisposedAsyncWaitHandle_CompletesCallback()
+    {
+        using Control control = new();
+        Assert.NotEqual(IntPtr.Zero, control.Handle);
+        bool callbackInvoked = false;
+        IAsyncResult asyncResult = control.BeginInvoke(() => callbackInvoked = true);
+        asyncResult.AsyncWaitHandle.Dispose();
+
+        Application.DoEvents();
+
+        Assert.True(callbackInvoked);
+        Assert.True(asyncResult.IsCompleted);
+    }
+
+    [WinFormsFact]
     public void Control_Invoke_Action_calls_correct_method()
     {
         using Control control = new();
